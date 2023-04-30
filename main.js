f9c094538387ca4bfaca45c0103dbb153aa9fca4
rusmansutardi <101523097+mansitee@users.noreply.github.com>
2023-04-30 21:28:40 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,125 @@
+// url :- `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=thor&key=[YOUR_API_KEY]'
+
+const apiKey = `AIzaSyDooaKG0vu4eWexLT-MlUVPpzXP8qqpfp4`
+
+let id
+
+let search = async () => {
+    let query = document.querySelector("#query").value
+
+    let url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${query}&key=${apiKey}`
+
+    let res = await fetch(url)
+
+    let data = await res.json()
+    console.log(data)
+    append(data.items)
+}
+
+
+let append = (data) =>{
+    let container = document.querySelector("#container")
+
+    container.innerHTML = null
+
+    data.forEach(({id : {videoId}, snippet: {title},snippet:{channelTitle} ,snippet: {thumbnails: {medium:{url}}}}) => {
+
+        let box = document.createElement("div")
+        // let iframe = document.createElement('iframe')
+        // iframe.src = `https://www.youtube.com/embed/${videoId}`
+        
+        let thumbnail = document.createElement("img")
+        thumbnail.src = url
+        thumbnail.setAttribute("class",'thumbnail')
+        
+        let name = document.createElement("p")
+        name.innerText = title
+        name.setAttribute("class", 'vidName')
+        
+        let channelName = document.createElement('p')
+        channelName.innerText = channelTitle
+        channelName.setAttribute("class", 'channelName')
+        
+        let video = {
+            title,
+            videoId
+        }
+
+        box.onclick = () =>{
+            playVideo(video)
+        }
+
+        box.append(thumbnail, name, channelName)
+        container.append(box)
+    })
+}
+
+let playVideo = (video) => {
+    localStorage.setItem("video", JSON.stringify(video))
+
+    window.location.href = 'video.html'
+}
+
+let trending = async () => {
+    let trendingUrl = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=20&key=${apiKey}`
+    // console.log(trendingUrl)
+
+    let trendingRes = await fetch(trendingUrl)
+    let trendingData = await trendingRes.json()
+
+    console.log(trendingData)
+    trendingAppend(trendingData.items)
+}
+// trending()
+
+let trendingAppend = (details) => {
+    let container = document.querySelector("#container")
+
+    container.innerHTML = null
+
+    details.forEach(({id , snippet: {title},snippet:{channelTitle} ,snippet: {thumbnails: {medium:{url}}}}) => {
+
+        let box = document.createElement("div")
+        
+        // let iframe = document.createElement('iframe')
+        // iframe.src = `https://www.youtube.com/embed/${videoId}`
+        
+        let thumbnail = document.createElement("img")
+        thumbnail.src = url
+        thumbnail.setAttribute("class",'thumbnail')
+        
+        let name = document.createElement("p")
+        name.innerText = title
+        name.setAttribute("class", 'vidName')
+        
+        let channelName = document.createElement('p')
+        channelName.innerText = channelTitle
+        channelName.setAttribute("class", 'channelName')
+        
+        let video = {
+            title,
+            id
+        }
+
+        box.onclick = () =>{
+            playVideo(video)
+        }
+
+        box.append(thumbnail, name, channelName)
+        container.append(box)
+    })
+}
+
+trending()
+
+
+document.querySelector("#profile").onclick = () =>{
+    profileFun()
+}
+
+let profileFun = () => {
+    alert("Create an Account")
+}
+// function profileFun(){
+//     alert('Create an Account')
+// }
---
+++ video.js
@@ -0,0 +1,44 @@
+let videoData = JSON.parse(localStorage.getItem("video"))
+
+// console.log(videoData.videoId)
+let videoId = videoData.videoId || videoData.id
+
+let videoAppend = (videoData) => {
+    console.log(videoId)
+    let iframe = document.createElement('iframe')
+    iframe.src = `https://www.youtube.com/embed/${videoId}`
+    iframe.allow = 'fullscreen'
+    iframe.allow = 'picture-in-picture'
+    iframe.allow = 'accelerometer'
+    iframe.width = '560'
+    iframe.height = '255'
+    iframe.frameborder = '0'
+    iframe.setAttribute('id', 'iframe')
+
+    document.querySelector('#vidContainer').append(iframe)
+}
+
+videoAppend()
+
+document.querySelector("#profile").onclick = () =>{
+    profileFun()
+}
+
+let profileFun = () => {
+    alert("Create an Account")
+}
+/*
+
+
+
+<iframe width="560" height="255" 
+src="https://www.youtube.com/embed/Nnj3YFlUa3Q"
+title="YouTube video player"
+frameborder="0" 
+allow="accelerometer; 
+autoplay; clipboard-write; 
+encrypted-media; gyroscope; 
+picture-in-picture" 
+allowfullscreen>
+</iframe>
+*/
